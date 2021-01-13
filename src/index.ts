20a1e3293b4f5b7c4e4c13247b512dc63b7471c6
Bruno Krauß <info@brunokrauss.com>
2021-01-13 17:57:16 +0100

    feat(boilerplate): added backend boilerplate

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+// tslint:disable-next-line: no-var-requires
+require('dotenv-safe').config();
+import express, { Request, Response } from 'express';
+const port: number = Number(process.env.PORT);
+
+export const startServer = async () => {
+  try {
+    const app = express();
+    app.get('/', async (_: Request, res: Response) => {
+      res.send('Hello world');
+    });
+
+    const server = app.listen(port, () => console.log(`Server is running on port ${port}`));
+    return { server };
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+// tslint:disable-next-line: no-floating-promises
+startServer();
