e699e4122e4c1bf91b69d58bee2ff7a8b501dfd7
Abdurrahman Yigen <stabyige@stud.h-da.de>
2021-01-18 19:35:30 +0100

    #4 Added database connection

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 // tslint:disable-next-line: no-var-requires
 require('dotenv-safe').config();
 import express, { Request, Response } from 'express';
+import "reflect-metadata"
+import { createDatabaseConnection } from './util/createDatabaseConnection';
+import * as bodyParser from 'body-parser';
 const port: number = Number(process.env.PORT);
 
 export const startServer = async () => {
   try {
-    const app = express();
+    const app = express()
+    const dbConnection = await createDatabaseConnection();
+    app.use(bodyParser.json());
     app.get('/', async (_: Request, res: Response) => {
-      res.send('Hello world');
+      res.send('Hello worldsssss');
     });
 
     const server = app.listen(port, () => console.log(`Server is running on port ${port}`));
-    return { server };
+    return { server, dbConnection };
   } catch (e) {
     console.log(e);
     throw e;
