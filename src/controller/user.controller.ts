1456c8766f783663cacc3a8cc7ada30391dda695
Pham Duc Le Huy <pham.huy@stud.h-da.de>
2021-01-19 23:57:22 +0100

    feat: add CRUD for EventType

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
+import { EventType } from "../entity/EventType";
 import { User } from "../entity/User";
 
 export const createUser = async (req: Request, res: Response) => {
@@ -21,6 +22,66 @@ export const createUser = async (req: Request, res: Response) => {
         })
     }
 }
+
+//Create Event to user
+export const createEvent = async(req:Request,res:Response) => {
+    let userid = req.params.id;
+    let {title,description,duration,link} = req.body;
+    const userRepository = getRepository(User);
+    try{
+        let foundUser = await userRepository.findOneOrFail(userid)
+        let newEvent = new EventType()
+        newEvent.title = title
+        newEvent.description = description
+        newEvent.duration = duration
+        newEvent.link = link        
+        newEvent.user = foundUser
+        foundUser.eventTypes = [newEvent]
+        await getRepository(EventType).save(newEvent)
+        res.send("Successfully Added Event bedinhcao" )
+    }
+    catch(e){
+        res.status(404).send({status:'not found'})
+    }
+
+}
+
+//Update Event 
+export const updateEvent = async (req:Request,res:Response) => {
+    let eventid = req.params.eventid;
+    let {title,description,duration,link} = req.body
+    const eventRepository = getRepository(EventType)
+    try {
+        let foundEvent = await eventRepository.findOneOrFail(eventid)
+        foundEvent.title = title
+        foundEvent.duration =duration
+        foundEvent.link = link
+        foundEvent.description = description
+        const updatedEvent = await eventRepository.save(foundEvent)
+        res.send({
+            data:updatedEvent
+        })
+    }
+    catch(e){
+        res.status(404).send({status : 'not Found'});
+    }
+
+}
+
+//Delete Event
+export const DeleteEvent = async(req:Request,res:Response) => {
+    let eventid = req.params.eventid;
+    const eventRepository = getRepository(EventType)
+    try {
+        let foundEvent = await eventRepository.findOneOrFail(eventid)
+        await eventRepository.remove(foundEvent)
+        res.send("Deleted Successfully")
+    }
+    catch(e){
+    res.status(404).send({status : 'not Found'});
+    }
+}
+
 
 // export const getUserById = async (req: Request, res: Response) => {
 
--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,6 +1,16 @@
 import { Router } from "express";
-import { createUser } from "../controller/user.controller";
+import { createEvent, createUser, DeleteEvent, updateEvent } from "../controller/user.controller";
 
 export const userRouter = Router({ mergeParams: true });
 
 userRouter.post('/', createUser);
+
+//Add Event to user
+userRouter.post('/:userid/eventType', createEvent)
+
+//Update Event
+userRouter.patch('/:userid/eventType/:eventid', updateEvent)
+
+//Delete Event
+userRouter.delete('/:userid/eventType/:eventid', DeleteEvent)
+
