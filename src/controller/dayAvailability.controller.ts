e9b109c31ebf870a1abd346b4ad0d70fc2960108
Pham Duc Le Huy <pham.huy@stud.h-da.de>
2021-02-15 20:31:29 +0100

    Fix: remove redundant Route

--- src/controller/dayAvailability.controller.ts
+++ src/controller/dayAvailability.controller.ts
@@ -49,45 +49,6 @@ export const getDefaultWeek = (): DayAvailability[] => {
     return week;
 }
 
-export const createDayAvailabitily = async (req: Request, res: Response) => {
-    const { day, fromTimeHour, endTimeHour, fromTimeMinute, endTimeMinute } = req.body;
-    const availableTimeRepository = getRepository(DayAvailability);
-    const dayAvailability = new DayAvailability();
-
-    dayAvailability.day = day;
-    dayAvailability.fromTimeHour = fromTimeHour;
-    dayAvailability.endTimeHour = endTimeHour;
-    dayAvailability.fromTimeMinute = fromTimeMinute;
-    dayAvailability.endTimeMinute = endTimeMinute;
-    try {
-        const savedAvailableTime = await availableTimeRepository.save(dayAvailability);
-        res.send({
-            data: savedAvailableTime,
-        })
-    } catch (error) {
-        console.log("availableTimeController: create:", error)
-        res.status(500).send();
-    }
-
-}
-
-export const deleteDayAvailability = async (req: Request, res: Response) => {
-    const availableTimeId = req.params.availableTimeId;
-    const availableTimeRepository = getRepository(DayAvailability);
-
-    try {
-        const dayAvailability = await availableTimeRepository.findOneOrFail(availableTimeId);
-        await availableTimeRepository.remove(dayAvailability);
-        res.send({
-            message: "deleted_succesfully"
-        })
-    } catch (error) {
-        console.log("availableTimeController: delete:", error);
-        res.status(404).send({
-            status: "not_found"
-        });
-    }
-}
 
 export const patchDayAvailability = async (req: Request, res: Response) => {
     const userId = req.params.userId;
