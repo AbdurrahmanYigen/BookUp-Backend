4ab8c5906b609c7576a7d9c3489261af3f2fa537
Bruno Krauß <info@brunokrauss.com>
2021-02-18 11:16:42 +0100

    feat(seeds) added profilePhoto seeds

--- src/database/factories/ProfilePhoto.factory.ts
+++ src/database/factories/ProfilePhoto.factory.ts
@@ -2,9 +2,9 @@ import Faker from 'faker';
 import { define } from 'typeorm-seeding';
 import { ProfilePhoto } from "../../entity/ProfilePhoto";
 
-define(ProfilePhoto, (faker: typeof Faker) => {
+define(ProfilePhoto, (_faker: typeof Faker) => {
     const profilePhoto = new ProfilePhoto()
-    profilePhoto.url = faker.image.imageUrl();
+    profilePhoto.url = 'https://loremflickr.com/320/240';
 
     return profilePhoto;
 });
--- src/database/factories/User.factory.ts
+++ src/database/factories/User.factory.ts
@@ -1,18 +1,17 @@
 import Faker from 'faker';
-import { define } from 'typeorm-seeding';
+import { define, factory } from 'typeorm-seeding';
 import { User } from "../../entity/User";
 import { getDefaultWeek } from "../../controller/dayAvailability.controller";
+import { ProfilePhoto } from '../../entity/ProfilePhoto';
 
 
-define(User, (faker: typeof Faker) => {  
-    const hashedPassword: string = 'hash';
-    const email = faker.internet.email();
-
+define(User, (faker: typeof Faker) => {
     const user = new User()
     user.userName = faker.internet.userName();
-    user.password = hashedPassword;
-    user.email = email;
+    user.email = faker.internet.email();
+    user.password = 'hash';
     user.availableTime = getDefaultWeek();
+    user.imageId = factory(ProfilePhoto)() as any;
 
     return user;
 });
